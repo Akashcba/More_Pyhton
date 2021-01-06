e1008a88552045788c6f546ec36f571b5a64c463
Akash Choudhary <74610406+Akashcba@users.noreply.github.com>
2021-01-07 00:58:04 +0530

    ex4.py added

--- ex4.py
+++ ex4.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+#Handling advanced command line arguments using argparse module
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('integers',metavar='akash',type=str,nargs='+')
+parser.add_argument('-f',help='foo help')
+parser.add_argument('-b',help='bar help')
+parser.add_argument('bz',help='baz help')
+parser.add_argument('-t','--turn-on',action='store_true')
+parser.add_argument('-x','--exclude',action='store_false')
+parser.add_argument('-s','--start',action='store_true')
+args=parser.parse_args()
+
+print("The arguments ar as follows :\n",args)
